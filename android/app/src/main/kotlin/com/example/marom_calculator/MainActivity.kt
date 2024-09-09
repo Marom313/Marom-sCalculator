cca4fa2da4b3ed721380a9abdd227babeb3e67b9
Marom313 <95164282+Marom313@users.noreply.github.com>
2024-09-09 16:18:58 +0300

    All calculator ont commit!

---
+++ android/app/src/main/kotlin/com/example/marom_calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.marom_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/marom_colors.dart
@@ -0,0 +1,15 @@
+//Here Are all the colors contants
+
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromARGB(255, 31, 94, 243);
+const Color secondaryColor = Color.fromARGB(255, 251, 251, 11);
+
+const Color primaryColorDark = Color.fromARGB(67, 31, 95, 243);
+const Color secondaryColorDark = Color.fromARGB(105, 251, 251, 11);
+
+const Color backgroundColor = Color(0XFFFFE6AD);
+const Color headlineColor = Color(0XFF839BA6);
+const Color textColor = Color(0XFF7A5776);
+
+const Color white = Colors.white;
